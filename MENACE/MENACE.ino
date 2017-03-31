743d52e6ad02f9edf1b1d9232dc54fcd0a825229
LaizFigueroa <laizheck@hotmail.com>
2017-03-31 16:56:28 +0200

    New sketch with almost all methods needed and the initial stage of the switch statements.

--- MENACE/MENACE.ino
+++ MENACE/MENACE.ino
@@ -1,32 +1,38 @@
 #include <Smartcar.h>
 
-SR04 sensor;
+SR04 sensorFront;
+//SR04 sensorBack;
 Gyroscope gyro(6);
-const int TRIGGER_PIN = 51;
-const int ECHO_PIN = 50;
-unsigned int tempSpeed = 0;
-Car car;
+Odometer encoder; 
+Car car;  
 
-char data = 0;
+//Pin numbers       
+const int encoderPin = 2;     // the number of one of the odometers pin
+const int TRIGGER_PIN_F = 51;   // the number of the ultrasound sensor pin for the front
+const int ECHO_PIN_F = 50;      // the number of the ultrasound sensor pin for the front
+//const int TRIGGER_PIN_B = 51;   // the number of the ultrasound sensor pin for the back
+//const int ECHO_PIN_B = 50;      // the number of the ultrasound sensor pin for the back
+const int ledRight =  48;     // the number of the LED pin
+const int ledLeft =  49;      // the number of the LED pin
 
-const int ledLeft =  49; // the number of the LED pin
-const int ledRight =  48; // the number of the LED pin
-
-int ledState = LOW;             // ledState used to set the LED
-
-unsigned long previousMillisL = 0;        // will store last time LED was updated
-unsigned long previousMillisR = 0;
-
-const long interval = 100;           // interval at which to blink (milliseconds)
-
+//Variables used
+char data = 0;                // for the bluetooth connection
+unsigned int tempSpeed = 0;   // for setting the velocity
+int ledState = LOW;           // led state used to set the LED
+const long interval = 100;           // interval to blink (milliseconds)
+unsigned long previousMillisL = 0;   // to store last time LED at the left side was updated
+unsigned long previousMillisR = 0;   // to store last time LED at the right side was updated
 
 
 void setup() {
-sensor.attach(TRIGGER_PIN, ECHO_PIN);
+sensor.attach(TRIGGER_PIN_F, ECHO_PIN_F);
+//sensor.attach(TRIGGER_PIN_B, ECHO_PIN_B);
 Serial.begin(9600);
 gyro.attach();
+encoder.attach(encoderPin);
 car.begin(gyro);
 gyro.begin();
+encoder.begin();
 
 // initialize the digital pin as an output.
 pinMode(ledLeft, OUTPUT);
@@ -35,10 +41,12 @@ pinMode(ledRight, OUTPUT);
 }
 
 
-void blinkRight(){
+void blinkRight(){        //Method to blink the right light
+  
     unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillisR >= interval) {
+    
     // save the last time you blinked the LED
     previousMillisR = currentMillis;
 
@@ -48,15 +56,17 @@ void blinkRight(){
     } else {
       ledState = LOW;
     }
-    
     // set the LED with the ledState of the variable:
     digitalWrite(ledRight, ledState);
   }
 }
-void blinkLeft(){
+
+void blinkLeft(){       //Method to blink the left light
+  
     unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillisR >= interval) {
+    
     // save the last time you blinked the LED
     previousMillisL = currentMillis;
 
@@ -66,62 +76,83 @@ void blinkLeft(){
     } else {
       ledState = LOW;
     }
-   
     // set the LED with the ledState of the variable:
     digitalWrite(ledLeft, ledState);
   }
 }
-void blinkAlert(){
+
+void blinkAlert(){      //Method to make both lights blink when facing an obstacle
+  
     unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillisR >= interval && currentMillis - previousMillisL >= interval ) {
+    
     // save the last time you blinked the LED
     previousMillisL = currentMillis;
     previousMillisR = currentMillis;
+    
     // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
-    
     // set the LED with the ledState of the variable:
     digitalWrite(ledLeft, ledState);
     digitalWrite(ledRight, ledState);
-   
   }
 }
-void turnRight(){
-   car.rotate(1);
-    ledState = LOW;
+
+void turnRight(){       //Method to make the car turn right + blink the right light
+  
+   blinkRight();      //First blink
+   car.rotate(1);     //Rotate
+   ledState = LOW;
     // set the LED with the ledState of the variable:
-    digitalWrite(ledRight, ledState);
+   digitalWrite(ledRight, ledState);
 }
 
-void turnLeft(){
-   car.rotate(-1);
+void turnLeft(){        //Method to make the car turn left + blink the left light
+  
+   blinkLeft();     //First blink
+   car.rotate(-1);  //Rotate
    ledState = LOW;   
     // set the LED with the ledState of the variable:
-    digitalWrite(ledLeft, ledState);
+   digitalWrite(ledLeft, ledState);
 }
 
-void moveCar(int tempSpeed){
+void moveCar(int tempSpeed){  //Method to make the car move given a speed
   tempSpeed = tempSpeed;
     car.setSpeed(tempSpeed);
 }
 
-void stopCar(){
+void stopCar(){       //Method to make the car stop
   tempSpeed = 0;
   car.setSpeed(tempSpeed);
 }
 
-void goBack(int tempSpeed) {
+void goBack(int tempSpeed) {      //Method to make the car go backwards for a limited distance
+                                  //Maybe jsut for the autonomous mode
+  unsigned int distanceOd = encoder.getDistance();  //Geting the odometer distance not the sensor
    tempSpeed = tempSpeed;
    car.setSpeed(-(tempSpeed));
+   if(distanceOd > 5){
+    stopCar();
+   }
 }
 
-boolean Obstacle(){
-  unsigned int distance = sensor.getDistance();
+boolean ObstacleFront(){               // Method to identify if there is an obstacle or not in front of the car - Not tested after small changes
+  unsigned int distance = sensorFront.getDistance();
+  if (distance > 0 && distance < 20){
+    return true; 
+  }
+  else {
+    return false;
+  }
+}
+
+boolean ObstacleBack(){               // Method to identify if there is an obstacle or not in the back of the car - not tested
+  unsigned int distance = sensorBack.getDistance();
   if (distance > 0 && distance < 20){
     return true; 
   }
@@ -132,35 +163,28 @@ boolean Obstacle(){
 
 void loop() {
   
-unsigned int distance = sensor.getDistance();
-// since initially, the speed is set to 30,
-// the car will maintain that speed until
-// it encounters an obstacle
 if(Serial.available() > 0){  // Send data only when you receive data:
   data = Serial.read();
 
   switch (input){ //Needs the input from the App - to be done
   
-    case 'Auto': 
+    case 'Auto': //The case where the user selects the car to be autonomous
     
       switch (input){ //Needs the input from the App - to be done
-        case 'start': 
+        case 'start': //If the start button on the autonomous mode is selected
       
-          moveCar(30);
-          while(Obstacle() == true){
+          //moveCar(30); // This gives problem together with the other methods, but alone works fine
+          if(Obstacle()){
 
             blinkAlert();
-            stopCar();
-            goBack(15);
-            stopCar();
-            blinkRight();
-            turnRight();
+            goBack(50);
+            turnRight();  
           } 
           break;
-        case 'stop':  
+        case 'stop':  //If the stop button on the autonomous mode is selected
            stopCar();
            break;
-        case 'manual':
+        case 'manual': //If the manual button on the autonomous "page" is selected
             //to be done
             break;
       }
