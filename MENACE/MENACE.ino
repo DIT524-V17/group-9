b4eccad73f3e0581d08b363b20d1680dff1f3103
LaizFigueroa <laizheck@hotmail.com>
2017-04-01 14:54:15 +0200

    changes

--- MENACE/MENACE.ino
+++ MENACE/MENACE.ino
@@ -1,7 +1,7 @@
 #include <Smartcar.h>
 
 SR04 sensorFront;
-//SR04 sensorBack;
+SR04 sensorBack;
 Gyroscope gyro(6);
 Odometer encoder; 
 Car car;  
@@ -10,15 +10,14 @@ Car car;
 const int encoderPin = 2;     // the number of one of the odometers pin
 const int TRIGGER_PIN_F = 51;   // the number of the ultrasound sensor pin for the front
 const int ECHO_PIN_F = 50;      // the number of the ultrasound sensor pin for the front
-//const int TRIGGER_PIN_B = 51;   // the number of the ultrasound sensor pin for the back
-//const int ECHO_PIN_B = 50;      // the number of the ultrasound sensor pin for the back
+const int TRIGGER_PIN_B = 45;   // the number of the ultrasound sensor pin for the back
+const int ECHO_PIN_B = 44;      // the number of the ultrasound sensor pin for the back
 const int ledRight =  48;     // the number of the LED pin
 const int ledLeft =  49;      // the number of the LED pin
 
 //Variables used
-char data = 0;                // for the bluetooth connection
+char input = 0;                // for the bluetooth connection
 unsigned int tempSpeed = 0;   // for setting the velocity
-char input;                   // holds the input from the app
 int ledState = LOW;           // led state used to set the LED
 const long interval = 100;           // interval to blink (milliseconds)
 unsigned long previousMillisL = 0;   // to store last time LED at the left side was updated
@@ -27,9 +26,8 @@ unsigned long previousMillisR = 0;   // to store last time LED at the right side
 
 void setup() {
 sensorFront.attach(TRIGGER_PIN_F, ECHO_PIN_F);
-//sensor.attach(TRIGGER_PIN_B, ECHO_PIN_B);
+sensorBack.attach(TRIGGER_PIN_B, ECHO_PIN_B);
 Serial.begin(9600);
-//Serial.setTimeout(200);
 gyro.attach();
 encoder.attach(encoderPin);
 car.begin(gyro);
@@ -142,10 +140,18 @@ void goBack(int tempSpeed) {      //Method to make the car go backwards for a li
     stopCar();
    }
 }
+void goBackM(int tempSpeed) {      //Method to make the car go backwards for a limited distance
+                                  //Maybe jsut for the autonomous mode
+ unsigned int distanceOd = sensorBack.getDistance();  //Geting the odometer distance not the sensor
+   tempSpeed = tempSpeed;
+   car.setSpeed(-(tempSpeed));
+   if(distanceOd < 20 ){
+    stopCar();
+   }
+}
 
-// Method to identify if there is an obstacle or not in front of the car - Not tested after small changes
 
-boolean ObstacleFront(){               
+boolean ObstacleFront(){               // Method to identify if there is an obstacle or not in front of the car - Not tested after small changes
   unsigned int distance = sensorFront.getDistance();
   if (distance > 0 && distance < 20){
     return true; 
@@ -155,64 +161,74 @@ boolean ObstacleFront(){
   }
 }
 
-// Method to identify if there is an obstacle or not in the back of the car - not tested
-
-//boolean ObstacleBack(){               
-//  unsigned int distance = sensorBack.getDistance();
- // if (distance > 0 && distance < 20){
- //   return true; 
-//  }
- // else {
-  //  return false;
- // }
-//}
+boolean ObstacleBack(){               // Method to identify if there is an obstacle or not in the back of the car - not tested
+  unsigned int distance = sensorBack.getDistance();
+  if (distance > 0 && distance < 20){
+    return true; 
+  }
+  else {
+    return false;
+  }
+}
 
 void loop() {
   
 if(Serial.available() > 0){  // Send data only when you receive data:
-  data = Serial.read();
-  switch (input){ //Needs the input from the App - to be done
-  
-    case 'A': //The case where the user selects the car to be AUTONOMOUS
+  input = Serial.read();
+
+  switch (input){ 
+  // case 'Auto' from the control and from the mode
+    case 'A': //The case where the user selects the car to be autonomous
     
-      switch (input){   //Needs the input from the App - to be done
-        case 'S':   //If the START button on the autonomous mode is selected
+      switch (input){   
+        case 'S':   //If the start button on the autonomous mode is selected
       
           //moveCar(30); // This gives problem together with the other methods, but alone works fine
           if(ObstacleFront()){
 
             blinkAlert();
             goBack(50); //Do not change this number for lower - does not run the car
+            
+            if(ObstacleBack()){
+              blinkAlert();
+              turnLeft();
+            }
             turnRight();  
           } 
           break;
-        case 'P':  //If the STOP button on the autonomous mode is selected
+        case 'B':  //If the stop button on the autonomous mode is selected
            stopCar();
            break;
-        case 'M': //If the MANUAL button on the autonomous "page" is selected
-            //to be done
-            break;
+ 
       }
-    case 'M': // To be done; MANUAL
-      switch (input){    // Needs the input from the App - to be done
-        case 'F':       // FORWARD
+      //If the manual button is selected on both UI
+    case 'M': 
+      switch (input){   
+        case 'f':
           moveCar(30); 
-        case 'B':       // BACKWARD 
-          goBack(50);
-        case 'L':      // LEFT
+          break;
+        case 'b':
+          goBackM(50);
+          break;
+        case 'l':
           turnLeft();
-        case 'R':      // RIGHT
+          break;
+        case 'r':
           turnRight();
-        case 'P':      // STOP
+          break;
+        case 's': 
           stopCar();
-        case 'E':     // ALERT
+          break;
+        case 'a':
           blinkAlert();
-        //case 'Auto': //Needs to be done
-            
+          break;
+               
               
-  //  case 'Follow': // To be done
+    //case 'F': // To be done
     
-    }
-  }
+      }
+      }
+
 }
 }
+
