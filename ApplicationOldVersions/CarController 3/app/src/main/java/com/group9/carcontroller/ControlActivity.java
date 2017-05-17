5e922843b7363b33c10b95b378882355287f4202
gusuljni <gusuljni@student.gu.se>
2017-05-17 21:58:08 +0200

    Organized the files a bit

---
+++ ApplicationOldVersions/CarController 3/app/src/main/java/com/group9/carcontroller/ControlActivity.java
@@ -0,0 +1,230 @@
+package com.group9.carcontroller;
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ControlActivity extends AppCompatActivity {
+// test git
+    //Define buttons
+    Button btnUp,btnDown,btnStop,btnLeft,btnRight,btnBlink;
+    ToggleButton autonomousSwich;
+
+    boolean autonomousOn;
+
+    String address = null;
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //receive the address of the bluetooth device
+        Intent newint = getIntent();
+        address = newint.getStringExtra("ADDRESS");
+        autonomousOn=false;
+
+
+        new ConnectBT().execute(); //Call the class to connect
+
+        // Connect button to GUI
+        btnUp=(Button) findViewById(R.id.up);
+        btnDown=(Button) findViewById(R.id.down);
+        btnLeft=(Button) findViewById(R.id.left);
+        btnRight=(Button) findViewById(R.id.right);
+        btnStop=(Button) findViewById(R.id.stop);
+        btnBlink=(Button) findViewById(R.id.blink);
+
+        autonomousSwich=(ToggleButton) findViewById(R.id.autonomous);
+
+        // Set Button Action
+        btnUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAction("f");
+            }
+        });
+        btnDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAction("b");
+            }
+        });
+        btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAction("l");
+            }
+        });
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAction("r");
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAction("q");
+            }
+        });
+        btnBlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAction("j");
+            }
+        });
+
+        autonomousSwich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(autonomousOn){
+                    autonomousOn=false;
+                    btnDown.setActivated(false);
+                    btnUp.setActivated(false);
+                    btnLeft.setActivated(false);
+                    btnRight.setActivated(false);
+                    btnStop.setActivated(false);
+                    btnBlink.setActivated(false);
+                    setAction("s");
+                }else{
+                    //TURN it on
+                    autonomousOn=true;
+                    btnDown.setActivated(true);
+                    btnUp.setActivated(true);
+                    btnLeft.setActivated(true);
+                    btnRight.setActivated(true);
+                    btnStop.setActivated(true);
+                    btnBlink.setActivated(true);
+                    setAction("a");
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Take appropriate action for each action item click
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Disconnect();
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void Disconnect()
+    {
+        if (btSocket!=null) //If the btSocket is busy
+        {
+            try
+            {
+                btSocket.close(); //close connection
+            }
+            catch (IOException e){
+                Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_SHORT).show();
+            }
+        }
+        finish(); //return to the first layout
+    }
+
+
+    private void setAction(String actionChar)
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write(actionChar.getBytes());
+            }
+            catch (IOException e)
+            {
+                Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute()
+        {
+            progress = ProgressDialog.show(ControlActivity.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+            try
+            {
+                if (btSocket == null || !isBtConnected)
+                {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();//start connection
+                }
+            }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess)
+            {
+
+                Toast.makeText(getApplicationContext(),"Connection Failed. Is it a SPP Bluetooth? Try again",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"Connected",Toast.LENGTH_SHORT).show();
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
+    }
+
+
+}
